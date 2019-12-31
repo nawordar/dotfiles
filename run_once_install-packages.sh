a272abb12af8412d1003b4a8b48ebe61d5c8d495
Cezary Drożak <czarek@drozak.net>
2019-12-31 11:43:36 +0100

    WIP Install packages script

---
+++ run_once_install-packages.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# case $HOSTNAME in
+
+# # PC
+# nawordar-H81M-S1)
+#     export EDITOR=vim
+#     export TERMINAL=/usr/bin/xfce4-terminal
+#     ;;
+
+# # Notebook
+# cezary-notebook)
+#     export EDITOR=vim
+#     export TERMINAL=/usr/bin/kitty
+#     ;;
+# esac
