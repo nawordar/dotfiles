859cb0b56fb9643731898160c06f8e9e5dc8351b
nawordar <czarek@drozak.net>
2020-01-02 21:57:38 +0100

    Configure emacs

---
+++ private_dot_emacs.d/load.el
@@ -0,0 +1,67 @@
+;; This file is largely copied from http://ergoemacs.org/emacs/organize_your_dot_emacs.html
+
+(require 'package)
+;; (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+;;                     (not (gnutls-available-p))))
+;;        (proto (if no-ssl "http" "https")))
+;;   (when no-ssl
+;;     (warn "\
+;; Your version of Emacs does not support SSL connections,
+;; which is unsafe because it allows man-in-the-middle attacks.
+;; There are two things you can do about this warning:
+;; 1. Install an Emacs version that does support SSL and be safe.
+;; 2. Remove this warning from your init file so you won't see it again."))
+;;   ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+;;   ;; (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+;;   (add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+;;   (when (< emacs-major-version 24)
+;;     ;; For important compatibility libraries like cl-lib
+;;     (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+
+(package-initialize)
+
+(require 'ido)
+
+;; more stuff
+
+(defun xah-get-fullpath (@file-relative-path)
+  "Return the full path of *file-relative-path, relative to caller's file location.
+
+Example: If you have this line
+ (xah-get-fullpath \"../xyz.el\")
+in the file at
+ /home/mary/emacs/emacs_lib.el
+then the return value is
+ /home/mary/xyz.el
+Regardless how or where emacs_lib.el is called.
+
+This function solves 2 problems.
+
+① If you have file A, that calls the `load' on a file at B, and B calls `load' on file C using a relative path, then Emacs will complain about unable to find C. Because, emacs does not switch current directory with `load'.
+
+To solve this problem, when your code only knows the relative path of another file C, you can use the variable `load-file-name' to get the current file's full path, then use that with the relative path to get a full path of the file you are interested.
+
+② To know the current file's full path, emacs has 2 ways: `load-file-name' and `buffer-file-name'. If the file is loaded by `load', then `load-file-name' works but `buffer-file-name' doesn't. If the file is called by `eval-buffer', then `load-file-name' is nil. You want to be able to get the current file's full path regardless the file is run by `load' or interactively by `eval-buffer'."
+
+  (concat (file-name-directory (or load-file-name buffer-file-name)) @file-relative-path)
+)
+
+;; --------------------------------------------------
+;; load the plain emacs settings
+
+(load (xah-get-fullpath "settings"))
+
+;; --------------------------------------------------
+;; load packages
+
+(require 'ace-jump-mode)
+
+(require 'ergoemacs-mode)
+
+(require 'auctex-latexmk)
+
+;; --------------------------------------------------
+;; load files
+
+(load (xah-get-fullpath "keybinding.el"))
+(load (xah-get-fullpath "settings_packages.el"))
---
+++ private_dot_emacs.d/settings_packages.el
@@ -0,0 +1,26 @@
+;; === ergoemacs === ;;
+(setq ergoemacs-theme nil) ;; Uses Standard Ergoemacs keyboard theme
+(setq ergoemacs-keyboard-layout "us") ;; Assumes QWERTY keyboard layout
+(ergoemacs-mode 1)
+
+;; === latex === ;;
+(auctex-latexmk-setup)
+
+(add-hook 'LaTeX-mode-hook '(lambda ()
+		      (progn
+			(turn-on-reftex)
+			(LaTeX-math-mode  1)
+			(flyspell-mode    1)
+			(auto-fill-mode   1)
+			(setq fill-column 95)
+			(parenthesis-register-keys "{([$" LaTeX-mode-map)
+			(outline-minor-mode t)
+			(setq latex-mode-map LaTeX-mode-map)
+			(yas/reload-all)
+			(add-to-list 'TeX-command-list
+				     '("images" "utils.py svg2pdf"
+				       TeX-run-command nil t))
+			(add-to-list 'TeX-command-list
+				     '("make" "latexmk -pdf %s"
+				       TeX-run-TeX nil t
+				       :help "Run Latexmk on file")))))
