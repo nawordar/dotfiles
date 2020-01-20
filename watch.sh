7a1c1f19819d1720db8efb383774ac7fa2c9020e
nawordar <czarek@drozak.net>
2020-01-20 21:21:03 +0100

    Update watch script

--- watch.sh
+++ watch.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env sh
 # Script to automatically run `chezmoi apply` on file change. Requires inotify-tools to work
-exclude='^_|^\.|^#.+#$'
-inotifywait -m -e MODIFY \
-    @mld.svg \
-    @README.md \
-    @LICENSE \
-    --exclude "$exclude" \
-    **/* |
+exclude='^_|^\.|^#.+#$|mld\.svg|README\.md|LICENSE|watch\.sh'
+inotifywait -rm -e MODIFY \
+    * \
+    --exclude "$exclude" |
     while read path event file; do
         echo "File $path$file changed. Invoking \`chezmoi apply\`."
         chezmoi apply
