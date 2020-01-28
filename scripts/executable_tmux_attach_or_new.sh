0d66315eee5c5dbfd96308e8b0354e4c52863990
Cezary Drożak <czarek@drozak.net>
2020-01-28 22:01:26 +0100

    Fix tmux launch script

--- scripts/executable_tmux_attach_or_new.sh
+++ scripts/executable_tmux_attach_or_new.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-if tmux ls 2>&1 | grep -v '\(^no server running\|(attached)$\)' >/dev/null; then
+if tmux ls 2>&1 | grep -v '\(^no server running\|^error connecting\|(attached)$\)' >/dev/null; then
     tmux attach
 else
-    tmux
+    tmux new
 fi
