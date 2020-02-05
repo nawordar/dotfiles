2cd651219b89378f0ef88374934d7a769d2d3935
nawordar <czarek@drozak.net>
2020-02-05 18:44:06 +0100

    Add chemacs

---
+++ dot_emacs-profiles.el
@@ -0,0 +1,5 @@
+(("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("spacemacs" . ((user-emacs-directory . "~/clones/spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
+ ("doom" . ((user-emacs-directory . "~/clones/doom-emacs")
+            (env . (("DOOMDIR" . "~/.doom.d"))))))
