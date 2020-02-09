5974a3ea9b68cab45fc81462526164c9b6a16262
nawordar <czarek@drozak.net>
2020-02-09 17:53:33 +0100

    Add file variable

--- dot_emacs-profiles.el
+++ dot_emacs-profiles.el
@@ -3,3 +3,7 @@
                  (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
  ("doom" . ((user-emacs-directory . "~/clones/doom-emacs")
             (env . (("DOOMDIR" . "~/.doom.d"))))))
+
+;; Local Variables:
+;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
+;; End:
