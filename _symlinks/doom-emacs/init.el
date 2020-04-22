c2f64a87d277ead0d8c7fc13968eedb3769d17bd
Cezary Drożak <czarek@drozak.net>
2020-04-22 17:33:11 +0200

    Use Iosevka

--- _symlinks/doom-emacs/init.el
+++ _symlinks/doom-emacs/init.el
@@ -41,7 +41,8 @@
         +all             ; catch all popups that start with an asterix
         +defaults)       ; default popup rules
        (pretty-code
-        +fira)           ; replace bits of code with pretty symbols
+        +iosevka)
+        ;; +fira)           ; replace bits of code with pretty symbols
        ;;tabs              ; an tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
        unicode           ; extended unicode support for various languages
